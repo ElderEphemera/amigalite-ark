3137fca85ce29fa49ac9ed224f9b453cf666f215
Joshua Price <git@elderephemera.com>
2025-05-07 14:32:20 -0400

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,4 +1,11 @@
-with import <nixpkgs> {};
+{ pkgs ? import (builtins.fetchTarball {
+    name = "nixos-24.05";
+    url = "https://github.com/nixos/nixpkgs/archive/63dacb46bf9.tar.gz";
+    sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+  }) {}
+}:
+
+with pkgs;
 rec {
   version = "0.4";
   src = nix-gitignore.gitignoreSource ["*.nix"] ./.;
